558b117f4f45c941db8b6bf4f95eeb31a80676a6
LAPTOP-SQ03A8DH\Amber <Amberqu.q2@gmail.com>
2023-10-06 16:00:00 -0400

    player movement and animation done

--- Assets/Adventure/Scripts/AdventurePlayer.cs
+++ Assets/Adventure/Scripts/AdventurePlayer.cs
@@ -4,21 +4,62 @@ using UnityEngine;
 
 public class AdventurePlayer : MonoBehaviour
 {
-    float horizontalMove;
-    float verticalMove;
-    public float speed = 10f;
+    float moveSpeed = 3f;
 
-    SpriteRenderer myRend;
-    Animator myAnim;
+    Animator anim;
 
+    // Start is called before the first frame update
     void Start()
     {
-        myAnim = GetComponent<Animator>();
-        myRend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        
-    } 
+        Vector3 newPos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("running_up", true);
+
+            newPos.y += Time.deltaTime * moveSpeed;
+        }
+        else
+        {
+            anim.SetBool("running_up", false);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("running_down", true);
+
+            newPos.y -= Time.deltaTime * moveSpeed;
+        }
+        else
+        {
+            anim.SetBool("running_down", false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("running_side", true);
+
+            newPos.x += Time.deltaTime * moveSpeed;
+            gameObject.transform.localScale = new Vector3(2, 2, 2);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("running_side", true);
+
+            newPos.x -= Time.deltaTime * moveSpeed;
+            gameObject.transform.localScale = new Vector3(-2, 2, 2);
+        }
+        else
+        {
+            anim.SetBool("running_side", false);
+        }
+
+        transform.position = newPos;
+    }
 }
